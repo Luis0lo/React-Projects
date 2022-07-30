8f36be176202f0d74f1c64dda360bf2ec0197a88
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-07-30 18:08:36 +0100

    styling header

--- Redux Toolkit Todo-list/src/components/Header.js
+++ Redux Toolkit Todo-list/src/components/Header.js
@@ -1,10 +1,15 @@
 import { useSelector } from 'react-redux';
+import css from './header.module.css';
 
 const Header = () => {
   const todosLeft = useSelector((state) => state.todos.list.length);
   console.log(todosLeft);
 
-  return <div>Todos left: {todosLeft}</div>;
+  return (
+    <div className={css.container}>
+      <p>Todos left: {todosLeft}</p>
+    </div>
+  );
 };
 
 export default Header;
