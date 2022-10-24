b4c027440d62657ca63701feb76d4ef1a09d0b75
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-10-24 08:55:14 +0100

    created explore component

--- Multilanguage/src/App.js
+++ Multilanguage/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { LanguageProvider } from './containers/Language';
 
 import LanguageSelector from './components/LanguageSelector';
+import Explore from './components/Explore';
 
 function App() {
   return (
     <LanguageProvider>
       <div className="App">
         <LanguageSelector />
+        <Explore />
       </div>
     </LanguageProvider>
   );
---
+++ Multilanguage/src/components/Explore.js
@@ -0,0 +1,13 @@
+
+import { Text} from '../containers/Language';
+
+export default function Explore() {
+  return (
+    <div>
+      <p><Text tid="slogan" /></p>
+      <p><Text tid="title" /></p>
+      <p><Text tid="where" /></p>
+      <p><Text tid="country" /></p>
+    </div>
+  );
+}
